62805675c016d06083ae61eff5972f0fd4da579b
Ayano <s.correa.wilches@gmail.com>
2022-07-17 02:00:56 +0700

    Added supabase config

---
+++ cualAplicacion/app/services/supabase/supabase-config.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { createClient } from "@supabase/supabase-js"
+
+const { SUPABASE_URL, SUPABASE_ANON_KEY } = require("../../config/env")
+
+const supabaseUrl = SUPABASE_URL
+const supabaseAnonKey = SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  localStorage: AsyncStorage as any,
+  autoRefreshToken: true,
+  persistSession: true,
+  detectSessionInUrl: false,
+})
